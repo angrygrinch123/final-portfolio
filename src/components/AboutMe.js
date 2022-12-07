1a7adebd945c68ce8e58f4134f872d6ac36623ad
angrygrinch123 <115049595+angrygrinch123@users.noreply.github.com>
2022-12-06 21:01:47 -0500

    change about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -15,7 +15,7 @@ export default function AboutMe() {
       <Link href={pageData.aboutMe.advisor1.ref}  color="#A26769" underline="hover"> {pageData.aboutMe.advisor1.name} </Link> and
        <Link href={pageData.aboutMe.advisor2.ref} color="#A26769" underline="hover"> {pageData.aboutMe.advisor2.name}. </Link> 
        I like to <Typography display="inline" sx={{color:"#A26769"}}> redesign systems for users of varied stakeholders </Typography> to 
-      create interfaces that are more <Typography display="inline" sx={{color:"#A26769"}}> accessible and natural </Typography> for the users. 
+      create interfaces that are for <Typography display="inline" sx={{color:"#A26769"}}> social-good and are accessible and natural </Typography> for the users. 
     </Typography>
     </div>
   );
