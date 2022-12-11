a2bd0de0a28a36acbf4f313fbb34c3fec1f84ff1
angrygrinch123 <115049595+angrygrinch123@users.noreply.github.com>
2022-12-11 00:58:39 -0500

    change aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,14 +7,10 @@ export default function AboutMe() {
   return (
     <div id="about-me-container">
     <Typography align="left"  variant="subtitle1" sx={{fontWeight:'light'}}>
-      Hey, I'm {pageData.title}. I am a {pageData.aboutMe.occupation} at Brown University in the 
+      Hey, I'm {pageData.title}. I am a Computer Science Student and Researcher at Brown University in the 
       <Link href={pageData.aboutMe.advisor1.labRef} color="#53599A" underline="hover"> {pageData.aboutMe.advisor1.labName} </Link> and 
       <Link href={pageData.aboutMe.advisor2.labRef} color="#53599A" underline="hover"> {pageData.aboutMe.advisor2.labName}. </Link> 
  
-      I am co-advised by 
-      <Link href={pageData.aboutMe.advisor1.ref}  color="#53599A" underline="hover"> {pageData.aboutMe.advisor1.name} </Link> and
-       <Link href={pageData.aboutMe.advisor2.ref} color="#53599A" underline="hover"> {pageData.aboutMe.advisor2.name}. </Link> 
-      
       <p></p>
       I do research and <Typography display="inline" sx={{color:"#A26769"}}> redesign systems for users of varied stakeholders </Typography> to 
       create interfaces that are for <Typography display="inline" sx={{color:"#A26769"}}> social-good and are accessible and natural </Typography> for the users. 
